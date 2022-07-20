1d2254ef538715c989da0f7e9f9f96623549b880
Joshua Greer <jtgreer95@gmail.com>
2022-07-19 20:58:51 -0500

    need technologies

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
 function ProjectList({ projects }) {
-  console.log(projects);
+ 
+
+    const technologies = projects.technologies
+
+    const projectElement = projects.map((project) => {
+    return < ProjectItem key={project.id} />
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectElement}</div>
     </div>
   );
 }
